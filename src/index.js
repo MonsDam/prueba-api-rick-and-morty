b631e76cd41318e0384ddd744c0017d4d4cfec72
2021 DSM41 FATIMA MONSERRAT GARCIA DAMIAN <al222010694@gmail.com>
2022-04-08 23:37:39 -0500

    Some changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import Api_Routes from './Api_Routes'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// const elemento = (
+//     <div>
+//         <Api_Routes/>
+//     </div>
+//   )
+  
+//   const container = document.getElementById('root')
+  
+//   ReactDOM.render(elemento, container)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import { createRoot } from 'react-dom/client';
+import Api_Routes from './Api_Routes'   
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<Api_Routes/>);
